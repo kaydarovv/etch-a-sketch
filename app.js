9c6f6a6b12bad79981ac101c46e645ccbf4390c2
kaidarov <sanzharkaidarovv@gmail.com>
2023-12-22 22:04:31 +0600

    Add grid boxes coloring

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let gridContainer = document.querySelector("#grid");
 let numOfBoxes = 16;
 
+
 function createGrid(gridSize, gridContainer) {
-    for (let i = 1; i < gridSize**2 ; i++) {
+    for (let i = 1; i < gridSize**2 + 1 ; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.toggle('grid-square');
         gridSquare.style.width = `calc(100%/${numOfBoxes})`
@@ -15,5 +16,18 @@ function createGrid(gridSize, gridContainer) {
     gridContainer.appendChild(gridSquare);
 }
 
+
+
+
+
 // INITIALIZATION
 createGrid(numOfBoxes, gridContainer);
+
+let gridSquareList = document.querySelectorAll('.grid-square');
+
+gridSquareList.forEach(element => {
+    element.addEventListener('mouseover',(event) => {
+        console.log("cock");
+        element.style.backgroundColor = 'black';
+    });
+});
